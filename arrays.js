49dbf3c1d8fdce3191d796e971e7797908ec4184
jeffwbuchanan@gmail.com <jeffwbuchanan@gmail.com>
2018-02-26 00:55:13 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,5 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray (chocolateBars, chocolate){
-  return (chocolate, . . .chocolateBars);
+  return [chocolate, . . .chocolateBars];
 }
